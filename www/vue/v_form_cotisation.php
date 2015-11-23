6cb07547a6a8773a75ae12ece112300ccbd366ef
Gaelle <gaelle.romanet@gmail.com>
2015-11-23 11:01:57 +0100

    suppression de script BDAMIS

---
+++ www/vue/v_form_cotisation.php
@@ -0,0 +1,15 @@
+<div id="contenu">
+	<h3>Saisie cotisation annuelle</h3>
+		<form method="POST" action="">
+		<div class="">
+		
+			<fieldset>
+				<label>Montant de la cotisation :</label>
+				<input type="text">
+			</fieldset>
+			<div class="piedForm">
+				<input id="ok" type="submit" value="Valider" />
+				<input id="annuler" type="reset" value="Effacer" />
+			</div>
+		</div>
+		</form>
