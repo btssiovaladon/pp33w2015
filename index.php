cb8f3dc0183b4d8680d397a5b675dfccfb1c27d5
unknown <thomas.pouzargues@B511_04.BTSSIO.LOCAL>
2015-12-14 12:02:08 +0100

    maj

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-
  if(isset($_REQUEST['action'])){
 	$action = $_REQUEST['action'];
 	switch($action){
@@ -11,9 +10,3 @@
 ?>
 
 <a href="vue/v_ajout_amis.php">Ajout amis</a>
-
-
-include ('js/listeAutoComplet.php');
-
-?>
-
