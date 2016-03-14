7f1e0503a0b25e7a11ff42150e4b1eaf741123d9
unknown <nabil.haddad@B511_11.BTSSIO.LOCAL>
2016-03-14 11:40:20 +0100

    impression

--- vue/v_Tableau_des_actions.php
+++ vue/v_Tableau_des_actions.php
@@ -2,7 +2,7 @@
  
 	<form name="form_saisie_action" action="index.php?uc=Tableau_des_actions&action=choixlist" method="post">
 		<select name="list" onchange="this.form.submit()">
-			<option value="">Choisir une actions...</option>
+			<option value="">Choisir une action...</option>
 			<?php foreach ($listeAction as $ligne){ ?>
 				<option value="<?php echo $ligne['NUMACTION'];?>"><?php echo $ligne['NOMACTION'];?></option>
 			<?php } ?>	
@@ -30,7 +30,7 @@
 	<td><center><a href=" "><img src="image/	icone_consulter.png" width="32" height="32" /></a></center></td>
 	<td><center><a href=" "><img src="image/modifier.png" width="32" height="32" /></a></center></td>
 	<td><center><a href=" "><img src="image/delete-icon.png" width="32" height="32" /></a></center></td>
-	<td><center><a href=" "><img src="image/logo_imprimante.png" width="32" height="32" /></a></center></td>
+	<td><center><a href=" " "><img src="image/logo_imprimante.png" width="32" height="32" /></a></center></td>
 	<td><center><a href=" "><img src="image/etiquette-icone.png" width="32" height="32" /></a></center></td>
 </tr>
 </table>
